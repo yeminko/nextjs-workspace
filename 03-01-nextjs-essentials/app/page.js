302d6bcbe929337fb3f0d376560da4648a5572a6
yeminko <yeminko47@gmail.com>
2026-02-25 16:34:39 +0630

    Add initial package.json and logo image for Next.js application setup

---
+++ 03-01-nextjs-essentials/app/page.js
@@ -0,0 +1,14 @@
+import Header from "@/components/header";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main>
+      <Header />
+      <p>🔥 Let&apos;s get started! 🔥</p>
+      <p>
+        <Link href="/about">Learn more about us</Link>
+      </p>
+    </main>
+  );
+}
