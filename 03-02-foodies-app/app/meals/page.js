51ff284c3227b472a0916787dcd744f15a5cbb69
yeminko <yeminko47@gmail.com>
2026-03-09 19:30:10 +0630

    feat: enhance Community and Meals pages with new layouts and styles, add NavLink component for navigation

--- 03-02-foodies-app/app/meals/page.js
+++ 03-02-foodies-app/app/meals/page.js
@@ -1,3 +1,22 @@
+import Link from "next/link";
+import classes from "./page.module.css";
+
 export default function MealsPage() {
-  return <h1>Meals Page</h1>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>Choose your favorite meals from our menu</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share" className={classes.link}>
+            Share your own meal
+          </Link>
+        </p>
+      </header>
+      <main className={classes.main}></main>
+    </>
+  );
 }
