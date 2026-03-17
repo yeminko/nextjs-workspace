32c012ee3bab66a0a7422564052d824a005f8647
yeminko <yeminko47@gmail.com>
2026-03-17 15:50:23 +0630

    feat: implement MealDetailsPage layout and styles, add error and loading components

--- 03-02-foodies-app/app/meals/[mealSlug]/page.js
+++ 03-02-foodies-app/app/meals/[mealSlug]/page.js
@@ -1,3 +1,16 @@
+import Image from "next/image";
+import classes from "./page.module.css";
+
 export default function MealDetailsPage() {
-  return <h1>Meal Details</h1>;
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image fill />
+        </div>
+        <div className={classes.headerText}></div>
+      </header>
+      <main></main>
+    </>
+  );
 }
---
+++ 03-02-foodies-app/app/meals/error.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Error() {
+  return (
+    <main className="error">
+      <h1>Failed to load meals</h1>
+      <p>Sorry, we couldn&apos;t load the meals. Please try again later.</p>
+    </main>
+  );
+}
