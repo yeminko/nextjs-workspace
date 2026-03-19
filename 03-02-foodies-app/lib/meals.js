1fb59932ca15af0e807dbb1f0398ab5ac70a44fd
yeminko <yeminko47@gmail.com>
2026-03-19 14:10:10 +0630

    feat: implement MealDetailsPage and NotFound components, enhance ShareMealPage with form and image picker

--- 03-02-foodies-app/lib/meals.js
+++ 03-02-foodies-app/lib/meals.js
@@ -3,6 +3,10 @@ import sql from "better-sqlite3";
 const db = sql("meals.db");
 
 export async function getMeals() {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
+  await new Promise((resolve) => setTimeout(resolve, 500));
   return db.prepare("SELECT * FROM meals").all();
+}
+
+export function getMeal(slug) {
+  return db.prepare("SELECT * FROM meals WHERE slug = ?").get(slug);
 }
