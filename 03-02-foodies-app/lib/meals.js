0219866fc3a0931724679e7eb6d5bb0c5d750a15
yeminko <yeminko47@gmail.com>
2026-03-24 16:01:40 +0630

    feat: implement shareMeal functionality, enhance ShareMealPage with form action and image handling

--- 03-02-foodies-app/lib/meals.js
+++ 03-02-foodies-app/lib/meals.js
@@ -1,4 +1,7 @@
 import sql from "better-sqlite3";
+import slugify from "slugify";
+import xss from "xss";
+import fs from "node:fs";
 
 const db = sql("meals.db");
 
@@ -9,4 +12,30 @@ export async function getMeals() {
 
 export function getMeal(slug) {
   return db.prepare("SELECT * FROM meals WHERE slug = ?").get(slug);
+}
+
+export async function saveMeal(meal) {
+  meal.slug = slugify(meal.title, { lower: true });
+  meal.instructions = xss(meal.instructions);
+
+  const extension = meal.image.name.split(".").pop();
+  const filename = `${meal.slug}-${Date.now()}.${extension}`;
+
+  const stream = fs.createWriteStream(`public/images/${filename}`);
+  const bufferedImage = await meal.image.arrayBuffer();
+
+  stream.write(Buffer.from(bufferedImage), (error) => {
+    if (error) {
+      throw new Error("Failed to save image");
+    }
+  });
+
+  meal.image = `/images/${filename}`;
+
+  db.prepare(
+    `
+    INSERT INTO meals (title, summary, instructions, image, creator, creator_email, slug)
+    VALUES (@title, @summary, @instructions, @image, @creator, @creator_email, @slug)
+  `,
+  ).run(meal);
 }
