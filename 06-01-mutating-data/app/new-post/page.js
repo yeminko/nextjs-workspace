528f708d70f8d9b3dc75d1441d92589e71796739
yeminko <yeminko47@gmail.com>
2026-05-07 20:49:36 +0630

    feat: implement createPost function and update form action in NewPostPage

--- 06-01-mutating-data/app/new-post/page.js
+++ 06-01-mutating-data/app/new-post/page.js
@@ -1,8 +1,10 @@
 export default function NewPostPage() {
+  function createPost() {}
+
   return (
     <>
       <h1>Create a new post</h1>
-      <form>
+      <form action={createPost}>
         <p className="form-control">
           <label htmlFor="title">Title</label>
           <input type="text" id="title" name="title" />
