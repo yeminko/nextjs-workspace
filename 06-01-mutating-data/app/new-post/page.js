56fbf38a15ad228fba7338184ee2c10ea194283f
yeminko <yeminko47@gmail.com>
2026-05-11 15:37:19 +0630

    feat: implement createPost function with validation and add FormSubmit component for post submission

--- 06-01-mutating-data/app/new-post/page.js
+++ 06-01-mutating-data/app/new-post/page.js
@@ -1,5 +1,41 @@
+import FormSubmit from "@/components/form-submit";
+import { storePost } from "@/lib/posts";
+import { redirect } from "next/navigation";
+
 export default function NewPostPage() {
-  function createPost() {}
+  async function createPost(formData) {
+    "use server";
+    const title = formData.get("title");
+    const image = formData.get("image");
+    const content = formData.get("content");
+
+    let errors = [];
+
+    if (!title || title.trim().length === 0) {
+      errors.push("Title is required");
+    }
+
+    if (!content || content.trim().length === 0) {
+      errors.push("Content is required");
+    }
+
+    if (!image) {
+      errors.push("Image is required");
+    }
+
+    if (errors.length > 0) {
+      return {};
+    }
+
+    await storePost({
+      imageUrl: "",
+      title,
+      content,
+      userId: 1,
+    });
+
+    redirect("/feed");
+  }
 
   return (
     <>
@@ -23,8 +59,7 @@ export default function NewPostPage() {
           <textarea id="content" name="content" rows="5" />
         </p>
         <p className="form-actions">
-          <button type="reset">Reset</button>
-          <button>Create Post</button>
+          <FormSubmit />
         </p>
       </form>
     </>
