3ab9325fcc6c57edc675d157dc436f9acdd5d917
yeminko <yeminko47@gmail.com>
2026-05-13 14:45:09 +0630

    feat: create PostForm component for post submission with validation and error handling

--- 06-01-mutating-data/app/new-post/page.js
+++ 06-01-mutating-data/app/new-post/page.js
@@ -1,9 +1,9 @@
-import FormSubmit from "@/components/form-submit";
+import PostForm from "@/components/post-form";
 import { storePost } from "@/lib/posts";
 import { redirect } from "next/navigation";
 
 export default function NewPostPage() {
-  async function createPost(formData) {
+  async function createPost(prevState, formData) {
     "use server";
     const title = formData.get("title");
     const image = formData.get("image");
@@ -19,12 +19,12 @@ export default function NewPostPage() {
       errors.push("Content is required");
     }
 
-    if (!image) {
+    if (!image || image.size === 0) {
       errors.push("Image is required");
     }
 
     if (errors.length > 0) {
-      return {};
+      return { errors };
     }
 
     await storePost({
@@ -37,31 +37,5 @@ export default function NewPostPage() {
     redirect("/feed");
   }
 
-  return (
-    <>
-      <h1>Create a new post</h1>
-      <form action={createPost}>
-        <p className="form-control">
-          <label htmlFor="title">Title</label>
-          <input type="text" id="title" name="title" />
-        </p>
-        <p className="form-control">
-          <label htmlFor="image">Image URL</label>
-          <input
-            type="file"
-            accept="image/png, image/jpeg"
-            id="image"
-            name="image"
-          />
-        </p>
-        <p className="form-control">
-          <label htmlFor="content">Content</label>
-          <textarea id="content" name="content" rows="5" />
-        </p>
-        <p className="form-actions">
-          <FormSubmit />
-        </p>
-      </form>
-    </>
-  );
+  return <PostForm action={createPost} />;
 }
