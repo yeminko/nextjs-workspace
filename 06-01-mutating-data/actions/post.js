ff866740c8308a49afede46879bfb48db2b9dedb
yeminko <yeminko47@gmail.com>
2026-05-19 16:21:57 +0630

    feat: add togglePostLikeStatus function and implement optimistic UI updates for post likes

--- 06-01-mutating-data/actions/post.js
+++ 06-01-mutating-data/actions/post.js
@@ -1,7 +1,8 @@
 "use server";
 
 import { uploadImage } from "@/lib/cloudinary";
-import { storePost } from "@/lib/posts";
+import { storePost, updatePostLikeStatus } from "@/lib/posts";
+import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 export async function createPost(prevState, formData) {
@@ -45,4 +46,9 @@ export async function createPost(prevState, formData) {
   });
 
   redirect("/feed");
+}
+
+export async function togglePostLikeStatus(postId) {
+  await updatePostLikeStatus(postId, 2);
+  revalidatePath("/", "layout");
 }
