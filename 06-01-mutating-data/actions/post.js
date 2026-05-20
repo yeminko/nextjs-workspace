921db9b2993b637dc8c317c819b9238221cd0dd6
yeminko <yeminko47@gmail.com>
2026-05-20 15:56:25 +0630

    Add initial project setup with package.json and SVG assets
    - Created package.json for caching project with dependencies and scripts.
    - Added next.svg and vercel.svg files to the public directory for branding.

--- 06-01-mutating-data/actions/post.js
+++ 06-01-mutating-data/actions/post.js
@@ -44,7 +44,7 @@ export async function createPost(prevState, formData) {
     content,
     userId: 1,
   });
-
+  revalidatePath("/", "layout");
   redirect("/feed");
 }
 
---
+++ 07-01-configuring-cashing/app/messages/page.js
@@ -0,0 +1,14 @@
+import Messages from '@/components/messages';
+
+export default async function MessagesPage() {
+  const response = await fetch('http://localhost:8080/messages', {
+    cache: 
+  });
+  const messages = await response.json();
+
+  if (!messages || messages.length === 0) {
+    return <p>No messages found</p>;
+  }
+
+  return <Messages messages={messages} />;
+}
