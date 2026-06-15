fdd2b9a2128923f4f5d38aea172990f2301a0c8f
yeminko <yeminko47@gmail.com>
2026-06-15 08:44:50 +0630

    feat: implement login functionality and session management; add logout feature

--- 09-01-user-authentication/actions/auth-actions.js
+++ 09-01-user-authentication/actions/auth-actions.js
@@ -1,8 +1,8 @@
 "use server";
 
-import { createAuthSession } from "@/lib/auth";
+import { createAuthSession, destroySession } from "@/lib/auth";
 import { hashUserPassword } from "@/lib/hash";
-import { createUser } from "@/lib/user";
+import { createUser, getUserByEmail } from "@/lib/user";
 import { redirect } from "next/navigation";
 
 export async function signup(prevState, formData) {
@@ -33,4 +33,44 @@ export async function signup(prevState, formData) {
     }
     throw err;
   }
+}
+
+export async function login(prevState, formData) {
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  const existingUser = getUserByEmail(email);
+
+  if (!existingUser) {
+    return {
+      errors: {
+        email: "Could not authenticate user, please check your credentials.",
+      },
+    };
+  }
+
+  const isValidPassword = verifyPassword(existingUser.password, password);
+
+  if (!isValidPassword) {
+    return {
+      errors: {
+        password: "Could not authenticate user, please check your credentials.",
+      },
+    };
+  }
+
+  await createAuthSession(existingUser.id);
+  redirect("/training");
+}
+
+export function auth(mode, prevState, formData) {
+  if (mode == "login") {
+    return login(prevState, formData);
+  }
+  return signup(prevState, formData);
+}
+
+export async function logout() {
+  await destroySession();
+  redirect("/");
 }
